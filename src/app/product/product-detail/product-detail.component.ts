c121433782c1cc94f011764d7159b5816032ad67
toyokuni <stoyokuni@ray.ocn.ne.jp>
2021-07-18 17:19:47 +0900

    Angularフロントエンド側作成

---
+++ src/app/product/product-detail/product-detail.component.ts
@@ -0,0 +1,25 @@
+import { typeWithParameters } from '@angular/compiler/src/render3/util';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../../products';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.scss']
+})
+export class ProductDetailComponent implements OnInit {
+  product: any;
+
+
+  constructor(private route: ActivatedRoute,) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get("productId")!]
+    }
+
+    )
+  }
+
+}
